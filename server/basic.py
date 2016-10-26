3d17fa3831eaba16d65ad32f8a1f0e0f61e0c0b0
Mark Liu <uilkram@gmail.com>
2016-10-26 01:15:28 +0000

    Upate counterpart table isntead of deleting row, enabling request re-sending

--- server/basic.py
+++ server/basic.py
@@ -1,7 +1,10 @@
 import os
+import json
 import sqlite3
+import uuid
 
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from datetime import datetime, date
 # from flask.ext.api import status
 
 app = Flask(__name__)
@@ -20,6 +23,19 @@ def connect_db():
         rv.row_factory = sqlite3.Row
         return rv
 
+def get_db():
+        """Opens a new database connection if there is none yet for the current application context.
+        """
+        if not hasattr(g, 'sqlite_db'):
+                g.sqlite_db = connect_db()
+        return g.sqlite_db
+
+def query_db(query, args=(), one=False):
+        cur = get_db().execute(query,args)
+        rv = cur.fetchall()
+        cur.close()
+        return (rv[0] if rv else None) if one else rv
+        
 def init_db():
         db = get_db()
         with app.open_resource('schema.sql', mode='r') as f:
@@ -31,50 +47,78 @@ def initdb_command():
         init_db()
         print('Initalized the database.')
 
-def get_db():
-        """Opens a new database connection if there is none yet for the current application context.
-        """
-        if not hasattr(g, 'sqlite_db'):
-                g.sqlite_db = connect_db()
-        return g.sqlite_db
-
 @app.teardown_appcontext
 def close_db(error):
         """Closes the database again at the end of the request."""
         if hasattr(g, 'sqlite_db'):
                 g.sqlite_db.close()
 
+# call when returning error from request_driver or request_rider.
+# log an error, flash a message, return dict with "matched" = false.
+def on_error(message):
+        print message
+        flash(message)
+        return json.dumps({"matched" : False}, ensure_ascii=False)
+
+# either update other table, matching an entry with my ID, or if no
+# entries "available" in other table, insert into my table.
+def update_other_table_or_add_to_my_table(coords, other_table, my_table):
+        id = str(uuid.uuid4())
+        # TODO: Instead of returning first option here, should try to do reasonable job of finding closest
+        # counterpart.
+        first_choice = query_db("SELECT * FROM {} WHERE counterpart_id IS NULL".format(other_table), one=True)
+        db = get_db()
+        if first_choice != None:
+                other_id = first_choice[0]
+                # update <other_table> table.    
+                db.execute("UPDATE {} SET counterpart_id = ? WHERE id = ?".format(other_table),
+                           (id, other_id))
+                db.commit()
+                message = 'Successfully updated ' + other_table
+                flash(message)
+                # return coordinates of counterpart.
+                coords_dict = {"matched" : True,
+                               "lat" : first_choice[1],
+                               "lon" : first_choice[2]}
+                print "found counterpart with id: " + other_id
+                return json.dumps(coords_dict, ensure_ascii = False) 
+        # add to <my_table> table.
+        db.execute('insert into {} (id, lat, lon, timestamp) values (?,?,?,?)'.format(my_table),                   
+                   [id, coords[0], coords[1], datetime.now()])
+        db.commit()
+        message = 'Added entry to ' + my_table
+        return on_error(message)        
+
+# when rider requests a driver, check drivers table to see if any drivers
+# available.  if so, return that driver. otherwise, let rider know they've
+# been added to waiting queue.
 @app.route("/noober/driver")
 def request_driver():
         if "coords" not in request.args:
-                return 'Missing coords query param'
+                return on_error('Missing coords query param')
         coords = request.args.get('coords').split(',')
-        if len(coords) != 2 or not isinstance(coords[0], (float, int)) or not isinstance(coords[1], (float, int)):
-                return 'coords incorrectly formatted'
-
-        db = get_db()
-        db.execute('insert into entries (lat, long) values (?,?)',
-                   [coords[0], coords[1]])
-        db.commit()
-        flash('New entry successfully posted')
-        return redirect(url_for('show_entries'))
-
-@app.route("/noober/show_entries")
-def show_entries():
-        db = get_db()
-        cur = db.execute('select lat, long from entries')
-        entries = cur.fetchall()
-        return render_template('show_entries.html', entries=entries)
+        if len(coords) != 2: 
+                return on_error('coords incorrectly formatted')
+        return update_other_table_or_add_to_my_table(coords, 'drivers', 'riders')
 
 @app.route("/noober/rider")
 def request_rider():
         if "coords" not in request.args:
-                return 'Missing coords query param'
+                return on_error('Missing coords query param')
         coords = request.args.get('coords').split(',')
-        if len(coords) != 2 or not isinstance(coords[0], (float, int)) or not isinstance(coords[1], (float, int)):                return 'coords incorrectly formatted'
-        # TODO: implement rider request logic.
-        return "Hello driver! Sending rider to you!"
+        if len(coords) != 2:
+                return on_error('coords incorrectly formatted')
+        return update_other_table_or_add_to_my_table(coords, 'riders', 'drivers')        
 
+@app.route("/noober/show_riders")
+def show_riders():
+        riders = query_db('select * from riders')
+        return render_template('show_entries.html', entries=riders)
+
+@app.route("/noober/show_drivers")
+def show_drivers():
+        drivers = query_db('select * from drivers')
+        return render_template('show_entries.html', entries=drivers)
 
         
 if __name__ == "__main__":
