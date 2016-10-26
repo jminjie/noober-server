eed04748413714fa7c18071d104de016a97581d9
Mark Liu <uilkram@gmail.com>
2016-10-26 01:15:28 +0000

    Upate counterpart table isntead of deleting row, enabling request re-sending

--- server/schema.sql
+++ server/schema.sql
@@ -1,5 +1,19 @@
-drop table if exists entries;
-create table entries (
+drop table if exists riders;
+drop table if exists drivers;
+
+create table riders (
+  id string not null primary key,
   lat float(10,6) not null,
-  long float(10,6) not null
+  lon float(10,6) not null,
+  timestamp string,
+  counterpart_id string
 );
+
+create table drivers (
+  id string not null primary key,
+  lat float(10,6) not null,
+  lon float(10,6) not null,
+  timestamp string,
+  counterpart_id string
+);
+
